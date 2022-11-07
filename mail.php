a26888cb442e4f58df9d3d5674a8cdd01bc16b9d
Smith Bhojak <hunardesignstudios@gmail.com>
2022-11-07 18:20:28 +0530

    Set PhpMailer

--- mail.php
+++ mail.php
@@ -8,18 +8,72 @@
     $to = "99sbhojak@gmail.com";
     // $cc = '';  
     $subject = "Mail From ".$name;
-    $txt =" Name : ". $name ."\r\n Number : " . $number . "\r\n Email : " . $email . "\r\n Message : " . $message  ;
+    $body =" Name : ". $name ."\r\n Number : " . $number . "\r\n Email : " . $email . "\r\n Message : " . $message  ;
     $headers = "From: contact@ilma.hunardesign.in" . "\r\n";
     // "CC: ";
     
     
-    if(mail($to,$subject,$txt,$headers)){
-        echo
+    // if(mail($to,$subject,$txt,$headers)){
+    //     echo
+    //     '<script type="text/javascript">
+    //         confirm("Message Sent")
+    //         history.back()
+    //     </script>';
+
+    // }
+    
+?>
+
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+   // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'ilma.hunardesign.in';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'contact';                     //SMTP username
+    $mail->Password   = 'Bhojak11it03';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom($email, $name);
+    $mail->addAddress('99sbhojak@gmail.com', 'Smith Bhojak');     //Add a recipient //Name is optional
+    
+
+    // //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $subject;
+    $mail->Body    = $body;
+    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo
         '<script type="text/javascript">
             confirm("Message Sent")
             history.back()
         </script>';
-
-    }
-    
-?>
+} catch (Exception $e) {
+     echo
+        '<script type="text/javascript">
+            confirm("Error Sending mail try again later")
+            history.back()
+        </script>';
+    //echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
